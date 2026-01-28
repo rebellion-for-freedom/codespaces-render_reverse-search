696541241b86e6428c08cdfc4e272784d5f365b1
Hacker114 <orrysander@gmail.com>
2026-01-28 09:38:30 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const compression = require('compression');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const target = 'https://orsons-snorlax.vercel.app';
+const target = 'https://rebel-search.onrender.com/';
 
 // Create proxy with optimized agent (connection pooling)
 const proxy = httpProxy.createProxyServer({
